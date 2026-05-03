061242d02f157c81fc33ec4fcf771dbaee06bd75
maksim.zhidkov <vjmax793@gmail.com>
2026-05-04 01:32:03 +0300

    feat: add hero section

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,15 +1,15 @@
 import './App.css';
 import { ErrorBoundary } from './components/error-boundary';
-
-import { ErrorButton } from './components/ui/error-button';
+import { Hero } from './components/hero';
 import React from 'react';
 
 class App extends React.Component {
   render(): React.ReactNode {
     return (
       <ErrorBoundary>
-        <ErrorButton />
-        <footer />
+        <Hero />
+
+        <footer className="rounded-t-lg border border-b-0 border-[var(--border)] bg-[var(--bg)] p-3"></footer>
       </ErrorBoundary>
     );
   }
