6b1b2f2ed2d8dda858ba20b4c9e0fabc076544a8
maksim.zhidkov <vjmax793@gmail.com>
2026-05-04 01:47:28 +0300

    feat: add search section

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,6 +1,9 @@
 import './App.css';
 import { ErrorBoundary } from './components/error-boundary';
 import { Hero } from './components/hero';
+import { SearchForm } from './components/search-form';
+import { ErrorButton } from './components/ui/error-button';
+import { Search } from './components/search';
 import React from 'react';
 
 class App extends React.Component {
@@ -9,7 +12,13 @@ class App extends React.Component {
       <ErrorBoundary>
         <Hero />
 
-        <footer className="rounded-t-lg border border-b-0 border-[var(--border)] bg-[var(--bg)] p-3"></footer>
+        <Search>
+          <SearchForm />
+        </Search>
+
+        <footer className="rounded-t-lg border border-b-0 border-[var(--border)] bg-[var(--bg)] p-3">
+          <ErrorButton />
+        </footer>
       </ErrorBoundary>
     );
   }
---
+++ class-components/src/components/search-form.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import { Button } from './ui/button';
+import PokeballIcon from '../assets/pokeball.svg?react';
+
+type SearchFormProps = Record<string, never>;
+
+type SearchFormState = {
+  searchString: string;
+};
+
+export class SearchForm extends React.Component<
+  SearchFormProps,
+  SearchFormState
+> {
+  constructor(props: SearchFormProps) {
+    super(props);
+
+    this.state = {
+      searchString: '',
+    };
+  }
+
+  render(): React.ReactNode {
+    return (
+      <form className="flex gap-3">
+        <input
+          className="flex-1 rounded-lg border-2 border-[var(--border)] px-3 py-1 transition-colors hover:border-[var(--accent-border)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-[var(--accent)]"
+          type="search"
+          placeholder="Search Input Field"
+        />
+        <Button
+          className="bg-red-400 hover:border-red-500 hover:bg-red-500"
+          type="submit"
+        >
+          <PokeballIcon className="w-6 scale-130" />
+        </Button>
+      </form>
+    );
+  }
+}
