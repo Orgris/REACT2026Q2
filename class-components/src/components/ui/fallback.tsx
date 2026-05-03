dc1dac6ca4d7210a2ce82f007e5d8e9490b713ca
maksim.zhidkov <vjmax793@gmail.com>
2026-05-04 01:53:33 +0300

    feat: improve fallback styling

--- class-components/src/components/ui/fallback.tsx
+++ class-components/src/components/ui/fallback.tsx
@@ -12,7 +12,7 @@ export class Fallback extends React.Component<FallbackProps> {
 
   render(): ReactNode {
     return (
-      <div>
+      <div className="flex grow flex-col items-center justify-center gap-1 bg-[var(--bg)] lg:gap-2 lg:p-8">
         <p>{this.props.errorMessage}</p>
         <p>Please try again later</p>
         <Button onClick={this.handleRefresh}>Refresh</Button>
