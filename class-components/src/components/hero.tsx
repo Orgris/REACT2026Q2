16bc8e72ba766e4009eea60ece830bc62aa40df2
maksim.zhidkov <vjmax793@gmail.com>
2026-05-04 01:52:43 +0300

    feat: improve hero text style

--- class-components/src/components/hero.tsx
+++ class-components/src/components/hero.tsx
@@ -6,11 +6,11 @@ export class Hero extends React.Component {
       <section className="rounded-b-lg border border-t-0 border-[var(--border)] bg-[var(--bg)] p-6 lg:p-8">
         <div>
           <h1>
-            <span className="bg-[var(--rss)] px-5 font-bold text-[var(--rss-text)]">
+            <span className="bg-[var(--rss)] px-5 font-extrabold text-[var(--rss-text)]">
               RS School
             </span>
             <span
-              className="pl-5 text-[var(--rss)] [-webkit-text-stroke:6px_#3264AF] [paint-order:stroke_fill] [text-stroke:6px_#3264AF]"
+              className="pl-5 text-[var(--rss)] [-webkit-text-stroke:10px_#3264AF] [paint-order:stroke_fill] [text-stroke:10px_#3264AF]"
               style={{ fontFamily: 'Pokemon Solid' }}
             >
               Pokédex
