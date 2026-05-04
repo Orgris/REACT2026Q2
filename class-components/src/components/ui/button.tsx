4c1c4480727fbad94c801b56fd551cfaeed5a8be
maksim.zhidkov <vjmax793@gmail.com>
2026-05-04 23:51:46 +0300

    feat: improve button styling

--- class-components/src/components/ui/button.tsx
+++ class-components/src/components/ui/button.tsx
@@ -11,7 +11,7 @@ export class Button extends React.Component<ButtonProps> {
     return (
       <button
         {...rest}
-        className={`inline-flex w-fit rounded border-2 border-transparent bg-[var(--accent-bg)] px-3 py-1 text-base font-[var(--mono)] text-[var(--accent)] transition-colors hover:border-[var(--accent-border)] hover:text-[var(--text-h)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--accent)] ${className}`}
+        className={`inline-flex w-fit rounded-lg border-2 border-transparent bg-[var(--accent-bg)] p-1 text-base font-[var(--mono)] text-[var(--accent)] transition ease-in hover:scale-105 hover:border-[var(--accent-border)] hover:text-[var(--text-h)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-[var(--accent)] active:scale-95 active:border-[var(--accent)] ${className}`}
       >
         {children}
       </button>
