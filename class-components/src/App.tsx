556db956ba6570cf39b2e241b6192186e35f108b
maksim.zhidkov <vjmax793@gmail.com>
2026-05-05 02:30:37 +0300

    feat: add card list component

--- class-components/src/App.tsx
+++ class-components/src/App.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ErrorBoundary } from './components/error-boundary';
 import { Hero } from './components/hero';
 import { SearchForm } from './components/search-form';
@@ -32,6 +33,10 @@ class App extends React.Component<AppProps, AppState> {
         <Search>
           <SearchForm onSearchChange={this.handleSearchChange} />
         </Search>
+
+        <ErrorBoundary>
+          <CardList query={this.state.query} />
+        </ErrorBoundary>
 
         <footer className="rounded-t-lg border border-b-0 border-[var(--border)] bg-[var(--bg)] p-3">
           <ErrorButton />
---
+++ class-components/src/components/card.tsx
@@ -0,0 +1,65 @@
+import React from 'react';
+import type { Pokemon } from '../api/search-api-types';
+import { Spinner } from './ui/spinner';
+
+type CardProps = {
+  pokemon: Pokemon;
+};
+
+type CardListState = {
+  loading: boolean;
+  error: string | null;
+};
+
+export class Card extends React.Component<CardProps, CardListState> {
+  constructor(props: CardProps) {
+    super(props);
+
+    this.state = {
+      loading: true,
+      error: null,
+    };
+  }
+
+  handleImageLoad = () => {
+    this.setState({ loading: false });
+  };
+
+  render(): React.ReactNode {
+    const { pokemon } = this.props;
+    const { loading } = this.state;
+
+    const src =
+      pokemon.sprites.other?.showdown?.front_default ??
+      pokemon.sprites.front_default ??
+      '';
+
+    return (
+      <div className="relative z-10 h-76 w-60 rounded-lg" key={pokemon.id}>
+        <div className="flex h-full w-full flex-col p-3">
+          <div className="flex h-[140px] flex-shrink-0 items-center justify-center">
+            {loading && (
+              <div className="absolute">
+                <Spinner className="w-35 text-[var(--text)]" />
+              </div>
+            )}
+
+            <img
+              src={src}
+              alt={`${pokemon.name} image`}
+              onLoad={this.handleImageLoad}
+            />
+          </div>
+          <div className="flex flex-col gap-1">
+            <p>№ {pokemon.id}</p>
+            <p className="text-2xl font-bold text-[var(--text-h)]">
+              {pokemon.name}
+            </p>
+            <p className="text-justify">{pokemon.description}</p>
+          </div>
+        </div>
+        <div className="absolute bottom-0 -z-10 h-3/4 w-full rounded-3xl bg-[var(--border)] shadow-xl"></div>
+      </div>
+    );
+  }
+}
