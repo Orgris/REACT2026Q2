b06c094fbfcf206bf5e5173cc99fccab366a98ce
maksim.zhidkov <vjmax793@gmail.com>
2026-05-17 23:24:36 +0300

    feat: add not-found page

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import { BrowserRouter, Routes, Route } from 'react-router';
 import Home from './components/pages/home';
+import NotFound from './components/pages/not-found';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -11,6 +12,7 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />}>
           <Route index element={<Home />} />
+          <Route path="*" element={<NotFound />} />
         </Route>
       </Routes>
     </BrowserRouter>
