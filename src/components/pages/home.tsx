66c89a904425fe3bd43e438a8299818e4524fcc6
maksim.zhidkov <vjmax793@gmail.com>
2026-05-18 01:52:43 +0300

    feat: add footer component

--- src/components/pages/home.tsx
+++ src/components/pages/home.tsx
@@ -5,6 +5,7 @@ import { Header } from '../header';
 import { Search } from '../search';
 import { SearchForm } from '../search-form';
 import { ErrorButton } from '../ui/error-button';
+import { Footer } from '../ui/footer';
 
 export function Home() {
   const [query, setQuery] = useLocalStorage('searchString');
@@ -25,9 +26,9 @@ export function Home() {
         <CardList key={query} query={query} />
       </ErrorBoundary>
 
-      <footer className="rounded-t-lg border border-b-0 border-[var(--border)] bg-[var(--bg)] p-6">
+      <Footer>
         <ErrorButton />
-      </footer>
+      </Footer>
     </ErrorBoundary>
   );
 }
