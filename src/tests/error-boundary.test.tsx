823c7e8df78d8992879d4ff2713b42bc3a2b6a11
maksim.zhidkov <vjmax793@gmail.com>
2026-05-26 00:39:02 +0300

    refactor: move error-boundary file

--- src/tests/error-boundary.test.tsx
+++ src/tests/error-boundary.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { ErrorBoundary } from '../components/error-boundary';
 import { ErrorButton } from '../components/ui/error-button';
+import { ErrorBoundary } from '../components/ui/error-boundary';
 
 const errorBoundary = (
   <ErrorBoundary>
