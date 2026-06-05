31a9631eb84bad1f7e4ea0adddee5857c35c74e0
maksim.zhidkov <vjmax793@gmail.com>
2026-06-05 14:56:17 +0300

    init: start React Forms task

---
+++ src/pages/home/home.tsx
@@ -0,0 +1,5 @@
+export function Home() {
+  return <main className="flex grow flex-col"></main>;
+}
+
+export default Home;
---
+++ vitest.config.ts
@@ -0,0 +1,38 @@
+/// <reference types="vitest" />
+
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+
+export default defineConfig({
+  plugins: [react()],
+
+  test: {
+    globals: true,
+    environment: 'jsdom',
+
+    setupFiles: ['./src/tests/setup.tsx', './src/tests/mocks.tsx'],
+
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'html'],
+      thresholds: {
+        global: {
+          statements: 80,
+          branches: 50,
+          functions: 50,
+          lines: 50,
+        },
+      },
+      include: ['src/**/*.{js,jsx,ts,tsx}'],
+      exclude: [
+        'src/**/*.test.{js,jsx,ts,tsx}',
+        'src/**/*.spec.{js,jsx,ts,tsx}',
+        'src/index.{js,jsx,ts,tsx}',
+        'src/setupTests.{js,ts}',
+        'src/test-utils.{ts,tsx}',
+        'src/**/*.d.ts',
+        'src/main.tsx',
+      ],
+    },
+  },
+});
